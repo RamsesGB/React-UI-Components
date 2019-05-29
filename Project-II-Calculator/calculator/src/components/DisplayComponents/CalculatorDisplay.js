0c1f93885a7d7a28e2a5762670d3f282884e602d
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-05-29 19:03:04 -0400

    Mvp complete project 2

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <div className='displayTotal'>0</div>
+    );
+};
+
+export default CalculatorDisplay;
